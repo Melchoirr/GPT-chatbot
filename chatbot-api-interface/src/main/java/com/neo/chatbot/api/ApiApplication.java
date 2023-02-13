15632d6d214188917b93cbf39d27934ef8b8ac62
Melchoirr <109651654+Melchoirr@users.noreply.github.com>
2023-02-13 13:18:43 +0800

    push poms1

---
+++ chatbot-api-interface/src/main/java/com/neo/chatbot/api/ApiApplication.java
@@ -0,0 +1,16 @@
+package com.neo.chatbot.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @description start the program
+ *
+ */
+
+@SpringBootApplication
+public class ApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiApplication.class, args);
+    }
+}
